662474ad4aa8b8e78505927ded462e7b3dab4305
gbellini90 <gbellini90@gmail.com>
2018-12-13 15:59:26 -0500

    deliverable 1 complete

--- src/index.js
+++ src/index.js
@@ -8,22 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
     // This is a general funtion that will take in an array, iterate the array and add each element.name, element front image and back image to html div
     array.forEach(function(pokemon){
       container.innerHTML += `<div class='pokemon'>${pokemon.name}
-                                <img src = '${pokemon.sprites.front}'>
-                                <img src = '${pokemon.sprites.back}'
+                                <img src = '${pokemon.sprites.front}' class='show'>
+                                <img src = '${pokemon.sprites.back}' class= 'hide'>
                               </div>`
     }
   )}
-
+//we have to call addPokemon on POKEMON array before we filter so that all the POKEMON will show
+  addPokemon(POKEMON)
 
   searchBar.addEventListener("input", function(event){
+    container.innerHTML = ''
     // Here we're paying attention to the input the user puts into the search bar
     const searchTerm = event.target.value
     // Whatever the value of the input is in the searchbar, we're setting as the variable "searchTerm"
 
     const filteredPokemons = POKEMON.filter(function(pokemon){ //Filtering through the POKEMON array
-      return pokemon.name.includes(searchTerm) // returns the name of the pokemon within the POKEMON array that matches the 
+      return pokemon.name.includes(searchTerm) // returns the name of the pokemon within the POKEMON array that matches the user input
     })
-      addPokemon(filteredPokemons)
+      addPokemon(filteredPokemons) //calls the method that will print the array in the HTML format (reference method above)
+
   })
 
 
